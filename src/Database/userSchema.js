b3d030871ad6e7b649bf29458c71a67235f0fb9d
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-06-28 07:42:52 +0300

    Added a mongo user schema

---
+++ src/Database/userSchema.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require('mongoose')
+const { ObjectId } = require('mongodb');
+
+const UserSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+    },   
+    hashedPass: 
+    { 
+        type: String, 
+        required: true 
+    },
+    rating:{
+        type: Number,
+        required: false
+    },
+    reviews:{
+        type: [ObjectId],
+        ref: 'review'
+    },
+    marketplaces:{
+        type: [ObjectId],
+        ref: 'reviews'
+    }
+})
