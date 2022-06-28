97f021f7b845626deedf5778396b272b9fd0fc9b
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-06-28 08:47:55 +0300

    Users are now successfully registered

--- src/API/userAPI/register.js
+++ src/API/userAPI/register.js
@@ -2,6 +2,8 @@ let userModel = require('../../Database/userSchema')
 
 const {checkUserExists} = require('./checkUserExists')
 
+let bcrypt = require('bcrypt')
+const saltRounds = 9
 
 async function registerUser(username, notHashedPassword)
 {
@@ -12,7 +14,14 @@ async function registerUser(username, notHashedPassword)
     }
     else
     {
-        // TODO: FINISH THE REGISTER ENDPOINT
+        let hashedPass =  await bcrypt.hash(notHashedPassword.toString(), saltRounds)
+        let user = await userModel({
+            username: username,
+            hashedPass: hashedPass,
+            rating: 0
+        })
+
+        await user.save()
         console.log(`registering user`)
     }
 }
