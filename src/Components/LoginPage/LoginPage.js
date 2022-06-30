a4f686b328bea9035f213cf8f06352a39af2fad6
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-06-30 09:08:49 +0300

    Added material UI to the project

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,11 +1,12 @@
 import Navbar from "../Navbar/navbar";
 import Footer from "../Footer/footer"
-
+import Button from '@mui/material/Button'
 export default function Login() {
     return (
     <div>
         <Navbar/>
         <h1>Test</h1>
+        <Button variant='contained'>Test button</Button>
         <Footer/>
     </div>);
 }
