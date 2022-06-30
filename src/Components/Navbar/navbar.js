9495775fc65d164280e1f2ee9a7a9892580c05ad
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-06-30 09:52:43 +0300

    navbar is now responsive to md displays

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,19 +4,42 @@ import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material";
 
 export default function Navbar() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
     return (
+        
     <div>
+        
         <Box sx={{ flexGrow: 1 }}>
             <AppBar color="warning" position="static">
-                <Toolbar>
-                    <Typography variant="h6">
-                        MarketPlace
-                    </Typography>
 
-                </Toolbar>
+                  { isMobile ? (
+                    <IconButton>
+                        <MenuIcon style={{color:"white"}}></MenuIcon>
+                    </IconButton>
+                    ) 
+                    :
+                    (
+                        <Toolbar>
+
+                            <Typography variant="h5" >
+                                MarketPlace 
+                            </Typography>
+
+                            <Typography variant="h5" >
+                                Buy 
+                            </Typography>
+
+                            <Typography variant="h5"     >
+                                Sell 
+                            </Typography>
+                        </Toolbar>
+
+                    )}
             </AppBar>
         </Box>
         <h1>Navbar</h1>
