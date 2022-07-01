6eab37c1fae07a8295a8c455219109fa87090ea0
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-01 12:14:34 +0300

    Made the page 404 message responsive

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,11 +1,16 @@
 import Navbar from "../Navbar/navbar";
 import Footer from "../Footer/footer"
+import './errorPage.css'
 
 export default function Error() {
     return(
     <div>
         <Navbar/>
-        <h2>Hi, I am an error page!</h2>
+        <div className="errorWrapper">
+            <h2>Error 404</h2>
+            <h3>The page you're looking for, does not exist</h3>
+
+        </div>
         <Footer/>
     </div>)
 }
