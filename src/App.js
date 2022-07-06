9b030d68d6269d37d8f9faa661167c3ac01d8508
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-06 16:49:35 +0300

    Brought back the app and express files. They were gone due to undoing the git add command...

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import  Login  from './Components/LoginPage/LoginPage'
+import  Error  from './Components/ErrorPage/ErrorPage'
+import  Home  from './Components/HomePage/homePage'
+import RegisterPage from './Components/RegisterPage/registerPage';
+// import {isAuthenticated} from './Express/express'
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<RegisterPage/>}></Route>
+
+        <Route path='*' element={<Error/>}></Route>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
