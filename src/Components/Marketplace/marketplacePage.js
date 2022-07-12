767d24917bbc9ab4988266170c97af74207466dd
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-12 17:06:16 +0300

    The marketplace data is successfully sent to the front end

--- src/Components/Marketplace/marketplacePage.js
+++ src/Components/Marketplace/marketplacePage.js
@@ -1,13 +1,45 @@
 import Navbar from "../Navbar/navbar";
 import Footer from "../Footer/footer"
+import Axios from 'axios'
+
+import {useState, useEffect} from 'react'
 
 export default function Marketplace ()
 {
+    // let targetMarketplaceData = getMarketplaceData()
+
+    const [marketplaceData, setMarketplaceData] = useState([])
+
+    useEffect(() => {
+        accessMarketplaceData()
+    }, [])
+
+    const accessMarketplaceData = async() => {
+        let targetMarketplace = await getMarketplaceData()
+        setMarketplaceData(targetMarketplace.data)
+    }
+
+    console.log(marketplaceData)
+
+
+
     return (
         <div>
             <Navbar/>
-                <h1>Marketplace data</h1>
+                <div>
+                    <h1></h1>
+                </div>
             <Footer/>
         </div>
     )
+}
+
+
+function getMarketplaceData()
+{
+    let marketplaceID = window.location.href.split('/')[4]
+
+    return Axios.post('http://localhost:3001/getMarketplaceById', {marketplaceID: marketplaceID}, {withCredentials: true})
+
+    
 }
