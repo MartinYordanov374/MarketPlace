d29d5ade9081809fabe12b8057f31f83b242e2c9
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-13 18:05:52 +0300

    Added styling to the review rating buttons

--- src/Components/Marketplace/marketplacePage.js
+++ src/Components/Marketplace/marketplacePage.js
@@ -7,6 +7,8 @@ import {Card, CardActionArea, CardContent, CardMedia, Typography, Button, CardAc
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import SellIcon from '@mui/icons-material/Sell';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 
 export default function Marketplace ()
 {
@@ -19,7 +21,6 @@ export default function Marketplace ()
     const [isUserOnProducts, setIsUserOnProducts] = useState(true)
 
     useEffect(() => {
-        console.log('use effect')
         async function getMarketplaceData()
         {
             let marketplaceID = window.location.href.split('/')[4]
@@ -88,74 +89,104 @@ export default function Marketplace ()
 
                     </div>
                     <Divider/>
-                            {
-                                isUserOnProducts == true ?
-                                <div className="marketplaceViewOptions">
-                                    <Card className="ProductsOption">
-                                        <CardActionArea sx={{color: "orange"}} onClick={() => handleMarketplaceView()}>
-                                            <h1>Products</h1>
-                                        </CardActionArea>
-                                    </Card>
-                                    <Card className="ReviewsOption">
-                                        <CardActionArea onClick={() => handleMarketplaceView()}>
-                                            <h1>Reviews</h1>
-                                        </CardActionArea>
-                                    </Card>
-                                </div>
-                                :
-                                <div className="marketplaceViewOptions">
-                                    <Card className="ProductsOption">
-                                        <CardActionArea onClick={() => handleMarketplaceView()}>
-                                            <h1>Products</h1>
-                                        </CardActionArea>
-                                    </Card>
-                                    <Card className="ReviewsOption">
-                                        <CardActionArea sx={{color: "orange"}} onClick={() => handleMarketplaceView()}>
-                                            <h1>Reviews</h1>
-                                        </CardActionArea>
-                                    </Card>
-                                </div>
-                            }
-
-                    {
-                    isUserOnProducts ? 
-                    <div className="marketplaceProducts">
-                    {
-                        marketplaceData.marketplaceProducts.length >= 1 ?
-                        marketplaceData.marketplaceProducts.map( (prod) => {
-                            return(
-                            <div class='marketplaceProduct'>
-                                <Card className='productCard'>
-                                    <CardActionArea>
-                                        <CardContent>
-                                            
-                                            <StorefrontIcon className="productImage"/>
-                                            <Divider/>
-
-                                            <h2 className="productName">{prod.productName}</h2>
-                                            <Divider/>
-
-                                            <h3 className="productDescription">{prod.productDescription}</h3>
-
-                                        </CardContent>
-                                        
+                    <div>
+                        {
+                            isUserOnProducts == true ?
+                            <div className="marketplaceViewOptions">
+                                <Card className="ProductsOption">
+                                    <CardActionArea sx={{color: "orange"}} onClick={() => handleMarketplaceView()}>
+                                        <h1>Products</h1>
                                     </CardActionArea>
-                                    <Divider/>
-                                    <p className="productPrice"><strong>$ {prod.productPrice}</strong></p>
-                                    <Divider/>
-                                    <Button className="buyProductButton" sx={{fontSize: 15}} color='warning'> <strong> Add to cart </strong> <AddShoppingCartIcon/>  </Button>
+                                </Card>
+                                <Card className="ReviewsOption">
+                                    <CardActionArea onClick={() => handleMarketplaceView()}>
+                                        <h1>Reviews</h1>
+                                    </CardActionArea>
                                 </Card>
                             </div>
-                            )
-                        })
+                            :
+                            <div className="marketplaceViewOptions">
+                                <Card className="ProductsOption">
+                                    <CardActionArea onClick={() => handleMarketplaceView()}>
+                                        <h1>Products</h1>
+                                    </CardActionArea>
+                                </Card>
+                                <Card className="ReviewsOption">
+                                    <CardActionArea sx={{color: "orange"}} onClick={() => handleMarketplaceView()}>
+                                        <h1>Reviews</h1>
+                                    </CardActionArea>
+                                </Card>
+                            </div>
+                        }
+                    </div>
+                    {
+                        isUserOnProducts ? 
+                        <div className="marketplaceProducts">
+                        {
+                            marketplaceData.marketplaceProducts.length >= 1 ?
+                            marketplaceData.marketplaceProducts.map( (prod) => {
+                                return(
+                                <div class='marketplaceProduct'>
+                                    <Card className='productCard'>
+                                        <CardActionArea>
+                                            <CardContent>
+                                                
+                                                <StorefrontIcon className="productImage"/>
+                                                <Divider/>
+
+                                                <h2 className="productName">{prod.productName}</h2>
+                                                <Divider/>
+
+                                                <h3 className="productDescription">{prod.productDescription}</h3>
+
+                                            </CardContent>
+                                            
+                                        </CardActionArea>
+                                        <Divider/>
+                                        <p className="productPrice"><strong>$ {prod.productPrice}</strong></p>
+                                        <Divider/>
+                                        <Button className="buyProductButton" sx={{fontSize: 15}} color='warning'> <strong> Add to cart </strong> <AddShoppingCartIcon/>  </Button>
+                                    </Card>
+                                </div>
+                                )
+                            })
+                            :
+                            <h1>No products here</h1>
+                        }
+                        </div>
                         :
-                        <h1>No products here</h1>
-                    }
-                    </div>
-                    :
-                    <div className="marketplaceReviews">
-                        <h1>Reviews</h1>
-                    </div>
+                        <div className="marketplaceReviews">
+                            
+                            { marketplaceData.marketplaceReviews.length >= 1 ?
+                                marketplaceData.marketplaceReviews.map((review) => {
+                                    console.log(review)
+                                    return (
+                                        <Card className="marketplaceReview">
+                                            <span className="reviewGiver">
+                                                <p>{review.reviewOwner.username}</p>
+                                            </span>
+                                            <span className="reviewRating">
+                                                <p>{review.reviewRating}</p>
+                                            </span>
+                                            <Divider/>
+                                            <span className="reviewContent">
+                                                <p>{review.reviewContent}</p>
+                                            </span>
+
+                                            <span className="reviewQuestionnaire">
+                                                <p>Is this review helpful to you?</p>
+                                                <ThumbUpIcon className="reviewHelpfulIcon"/>
+                                                <ThumbDownAltIcon className="reviewNotHelpfulIcon"/>
+
+                                            </span>
+                                        </Card>
+                                        
+                                    )
+                                })
+                                :
+                                <h1>No reviews to show</h1>
+                            }
+                        </div>
                     }
                 <Footer/>
             </div>
