aa10b8c75eb67857cb13f0e955be1c60fd91f471
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-13 18:29:25 +0300

    Changed the marketplace review helpfullness address

--- src/Express/express.js
+++ src/Express/express.js
@@ -390,11 +390,11 @@ async function startServer(){
 
     })
 
-    app.post('/reviewHelpful', async(req,res) => {
+    app.post('/marketplaceReviewHelpful', async(req,res) => {
 
     })
 
-    app.post('/reviewNotHelpful', async(req,res) => {
+    app.post('/marketplaceReviewNotHelpful', async(req,res) => {
         
     })
     //#region endpoints
