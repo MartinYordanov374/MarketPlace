cc08f617df44e42020cec02a11a8973ecafa4288
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-13 17:31:43 +0300

    The marketplace schema is now populated for the marketplace reviews

--- src/API/marketplaceAPI/getMarketplaceByID.js
+++ src/API/marketplaceAPI/getMarketplaceByID.js
@@ -2,7 +2,7 @@ let marketplaceModel = require('../../Database/marketplaceSchema')
 
 async function getMarketplaceById(marketplaceID){
     try{
-        let targetMarketplace = await marketplaceModel.findById({_id: marketplaceID}).populate('marketplaceOwner').populate('marketplaceProducts')
+        let targetMarketplace = await marketplaceModel.findById({_id: marketplaceID}).populate('marketplaceOwner').populate('marketplaceProducts').populate('marketplaceReviews')
         return targetMarketplace
     }
     catch(e){
