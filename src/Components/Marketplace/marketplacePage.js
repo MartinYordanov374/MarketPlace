e5da90ed9a69c1efeb21a49e0f0d20a5c2510ba0
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-13 18:24:08 +0300

    Changed the formula for calculating the review rating. It can be negative now

--- src/Components/Marketplace/marketplacePage.js
+++ src/Components/Marketplace/marketplacePage.js
@@ -168,7 +168,7 @@ export default function Marketplace ()
                                                 <a href={"/profile/" + review.reviewOwner._id} class="reviewGiverUsername">{review.reviewOwner.username}</a>
                                             </span>
                                             <span className="reviewRatingWrapper">
-                                                <p className="reviewRating">{Math.abs(review.positiveRatings - review.negativeRatings)} people found this review helpful!</p>
+                                                <p className="reviewRating">{review.positiveRatings.length - review.negativeRatings.length} people found this review helpful!</p>
                                             </span>
                                             <Divider/>
                                             <span className="reviewContent">
