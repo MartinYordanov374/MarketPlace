8bc3374f3681d37c70a1c95b72897e795518be34
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-07-28 15:40:39 +0300

    Completed the marketplace page refactoring

--- src/Components/Marketplace/marketplacePage.js
+++ src/Components/Marketplace/marketplacePage.js
@@ -1,14 +1,11 @@
 import Navbar from "../Navbar/navbar";
 import Footer from "../Footer/footer"
 import Axios from 'axios'
-import {useState, useEffect, useMemo, useRef} from 'react'
+import {useState, useEffect } from 'react'
 import './marketplaceStyles.css'
-import {Card, CardActionArea, CardContent, CardMedia, Typography, Button, CardActions, Link, Divider, Modal, Fade, Box, Input, TextField, TextareaAutosize} from '@mui/material'
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import {Card, CardActionArea, Typography, Button, Divider, Modal, Fade, Box} from '@mui/material'
+
 import SellIcon from '@mui/icons-material/Sell';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 import CircularProgress from '@mui/material/CircularProgress';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from "react-toastify";
@@ -17,8 +14,8 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import NotLoggedUser from "../NotLoggedUser/notLogged";
 import SettingsIcon from '@mui/icons-material/Settings';
 import ReviewModal from '../ReviewModal/reviewModal'
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import MarketplaceProduct from "../MarketplaceProduct/MarketplaceProduct";
+import MarketplaceReview from "../MarketplaceReview/MarketplaceReview";
 
 export default function Marketplace ()
 {
@@ -87,29 +84,6 @@ export default function Marketplace ()
         {
             setIsUserOnProducts(true)
         }
-    }
-
-    const reviewNotHelpfulHandler = (reviewID) => {
-
-        Axios.get('http://localhost:3001/getSessionData', {withCredentials: true})
-        .then((res) => {
-            let userID = res.data
-            Axios.post('http://localhost:3001/marketplaceReviewNotHelpful', {userID: userID, marketplaceID: marketplaceID, reviewID: reviewID}, {withCredentials: true})
-            .then((res) => {
-                toast.success(res.data)
-            })
-        })
-    }
-
-    const reviewHelpfulHandler = (reviewID) => {
-        Axios.get('http://localhost:3001/getSessionData', {withCredentials: true})
-        .then((res) => {
-            let userID = res.data
-            Axios.post('http://localhost:3001/marketplaceReviewHelpful', {userID: userID, marketplaceID: marketplaceID, reviewID: reviewID}, {withCredentials: true})
-            .then((res) => {
-                toast.success(res.data)
-            })
-        })
     }
 
     const openSettingsModal = () => {
@@ -309,38 +283,8 @@ export default function Marketplace ()
                             }
                             { marketplaceData.marketplaceReviews.length >= 1 ?
                                 marketplaceData.marketplaceReviews.map((review) => {
-                                    return (
-                                        <Card className="marketplaceReview">
-                                            {/* TODO: ADD PROFILE PICTURE TO THE REVIEW POST */}
-
-                                            <span className="reviewGiver">
-                                                {review.reviewOwner.profilePicture != undefined 
-                                                    ?
-                                                    <a href={"/profile/" + review.reviewOwner._id}>
-                                                        <img className='reviewGiverPfp' src={`data:image/jpg;base64, ${Buffer.from(review.reviewOwner.profilePicture).toString('base64')}`}></img>
-                                                    </a>
-                                                    :
-                                                    <PersonOutlineIcon className="reviewGiverIcon"/>
-                                                }
-                                                <a href={"/profile/" + review.reviewOwner._id} class="reviewGiverUsername">{review.reviewOwner.username}</a>
-                                            </span>
-                                            <span className="reviewRatingWrapper">
-                                                <p className="reviewRating">{review.positiveRatings.length - review.negativeRatings.length} review rating</p>
-                                            </span>
-                                            <Divider/>
-                                            <span className="reviewContent">
-                                                <p>{review.reviewContent}</p>
-                                            </span>
-
-                                            <Divider/>
-                                            <span className="reviewQuestionnaire">
-                                                <p>Was this review helpful?</p>
-                                                <ThumbUpIcon className="reviewHelpfulIcon" onClick={() => reviewHelpfulHandler(review._id)}/>
-                                                <ThumbDownAltIcon className="reviewNotHelpfulIcon" onClick={() => reviewNotHelpfulHandler(review._id)}/>
-
-                                            </span>
-                                        </Card>
-                                        
+                                    return ( 
+                                        <MarketplaceReview targetReview = { review } marketplaceData = { marketplaceData } /> 
                                     )
                                 })
                                 :
@@ -461,28 +405,7 @@ export default function Marketplace ()
                             marketplaceData.marketplaceProducts.length >= 1 ?
                             marketplaceData.marketplaceProducts.map( (prod) => {
                                 return(
-                                <div class='marketplaceProduct'>
-                                    <Card className='productCard'>
-                                        <CardActionArea>
-                                            <CardContent>
-                                                
-                                                <StorefrontIcon className="productImage"/>
-                                                <Divider/>
-
-                                                <h2 className="productName">{prod.productName}</h2>
-                                                <Divider/>
-
-                                                <h3 className="productDescription">{prod.productDescription}</h3>
-
-                                            </CardContent>
-                                            
-                                        </CardActionArea>
-                                        <Divider/>
-                                        <p className="productPrice"><strong>$ {prod.productPrice}</strong></p>
-                                        <Divider/>
-                                        <Button className="buyProductButton" sx={{fontSize: 15}} color='warning'> <strong> Add to cart </strong> <AddShoppingCartIcon/>  </Button>
-                                    </Card>
-                                </div>
+                                    <MarketplaceProduct prod = { prod }/>
                                 )
                             })
                             :
@@ -505,38 +428,7 @@ export default function Marketplace ()
                             { marketplaceData.marketplaceReviews.length >= 1 ?
                                 marketplaceData.marketplaceReviews.map((review) => {
                                     return (
-                                        <Card className="marketplaceReview">
-                                            <span className="reviewGiver">
-                                                {review.reviewOwner.profilePicture != undefined 
-                                                    ?
-                                                    <a href={"/profile/" + review.reviewOwner._id}>
-                                                        <img className='reviewGiverPfp' src={`data:image/jpg;base64, ${Buffer.from(review.reviewOwner.profilePicture).toString('base64')}`}></img>
-                                                    </a>
-                                                    :
-                                                    <PersonOutlineIcon className="reviewGiverIcon"/>
-                                                }
-                                                <a href={"/profile/" + review.reviewOwner._id} class="reviewGiverUsername">{review.reviewOwner.username}</a>
-                                            </span>
-                                            <span className="reviewRatingWrapper">
-                                                <p className="reviewRating">{review.positiveRatings.length - review.negativeRatings.length} review rating</p>
-                                            </span>
-                                            <Divider/>
-                                            <span className="reviewContent">
-                                                <p>{review.reviewContent}</p>
-                                            </span>
-
-                                            <Divider/>
-                                            {review.reviewOwner._id == marketplaceData.marketplaceOwner._id ? 
-                                                ""
-                                                :
-                                                <span className="reviewQuestionnaire">
-                                                    <p>Was this review helpful?</p>
-                                                    <ThumbUpIcon className="reviewHelpfulIcon" onClick={() => reviewHelpfulHandler(review._id)}/>
-                                                    <ThumbDownAltIcon className="reviewNotHelpfulIcon" onClick={() => reviewNotHelpfulHandler(review._id)}/>
-                                                </span>
-                                            }
-                                        </Card>
-                                        
+                                        <MarketplaceReview targetReview = { review } marketplaceData = { marketplaceData } /> 
                                     )
                                 })
                                 :
@@ -547,8 +439,10 @@ export default function Marketplace ()
                 </div>}
                 <Footer/>
             </div>
-        )
-        } 
+            )
+        }
+        
+        
         else
         {
             return (
